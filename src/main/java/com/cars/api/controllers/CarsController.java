1ffe4ccd8688b130c2f13d6d43f03365242e073d
Igor3550 <igormeloigormelo@gmail.com>
2023-03-30 21:58:25 -0300

    update router

--- src/main/java/com/cars/api/controllers/CarsController.java
+++ src/main/java/com/cars/api/controllers/CarsController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -32,6 +33,18 @@ public class CarsController {
   @GetMapping
   public List<Cars> listAll() {
     return repository.findAll();
+  }
+
+  @PutMapping("/{id}")
+  public void update(@PathVariable Long id, @RequestBody @Valid CarsDTO req){
+    repository.findById(id).map(car -> {
+      car.setModelo(req.modelo());
+      car.setAnoModelo(req.anoModelo());
+      car.setDataFabricacao(req.dataFabricacao());
+      car.setValor(req.valor());
+      car.setFabricante(req.fabricante());
+      return repository.save(car);
+    });
   }
 
   @DeleteMapping("/{id}")
